3bb7e338a3e4dd06cb96f9dd0f069d5dd69c097c
Qing <qing.sheng@thomsonreuters.com>
2016-11-14 11:59:19 +0800

    add package

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,21 @@ require('../less/style.less');
 
 
 require('angular');
+require('angular-animate');
+require('angular-touch');
 require('angular-ui-router');
 require('angular-ui-bootstrap');
 
 
 const dependancies = [
+    'ngAnimate',
+    'ngTouch',
     'ui.router',
     'ui.bootstrap'
 ];
 
 
-angular.module('qsmt',dependancies);
+angular.module('qsmt', dependancies);
 
 require('./config');
 require('./components');
