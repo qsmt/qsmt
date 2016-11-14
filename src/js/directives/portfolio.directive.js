af682f4f2db8c0c9ca48df52e9f3f6ecbfd3153b
Qing <qing.sheng@thomsonreuters.com>
2016-11-14 17:40:17 +0800

    update directive

--- src/js/directives/portfolio.directive.js
+++ src/js/directives/portfolio.directive.js
@@ -5,30 +5,34 @@
         .module('qsmt')
         .directive('portfolioItem', portfolioItem);
 
-    portfolioItem.$inject = [''];
+    portfolioItem.$inject = [];
+
     function portfolioItem() {
         // Usage:
         //
         // Creates:
         //
         var directive = {
-            template:'portfolioItem',
+            require: ['ngModel'],
+            template: require('./portfolio.directive.html'),
             bindToController: true,
             controller: ControllerController,
             controllerAs: 'vm',
             link: link,
-            restrict: 'A',
-            scope: {
-            }
+            restrict: 'AE',
+            scope: {}
         };
         return directive;
-        
-        function link(scope, element, attrs) {
+
+        function link(scope, element, attrs, controllers) {
+            var tabsCtrl = controllers[0];
+            console.log(tabsCtrl)
+            scope.model = tabsCtrl.$modelValue;
+            console.log(scope.model);
         }
     }
     /* @ngInject */
-    function ControllerController () {
-        console.log('ControllerController')
+    function ControllerController() {
+
     }
 })();
-
