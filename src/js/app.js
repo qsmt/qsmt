261c272d6fa23e8fd145d94f6454760a8c1b72c0
shengoo <shengoo@hotmail.com>
2017-03-26 22:59:27 +0800

    use react

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,13 @@
-require('bootstrap/less/bootstrap.less');
-require('../less/style.less');
+/**
+ * Created by sheng on 2017/3/5.
+ */
+// var React = require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import '../less/style.less';
 
-require('angular');
-require('angular-animate');
-require('angular-touch');
-require('angular-ui-router');
-require('angular-ui-bootstrap');
-
-
-const dependancies = [
-    'ngAnimate',
-    'ngTouch',
-    'ui.router',
-    'ui.bootstrap'
-];
-
-
-angular.module('qsmt', dependancies);
-
-require('./config');
-require('./components');
-require('./directives');
-require('./filters');
-require('./services');
-angular.module('qsmt').run(run);
-
-run.$inject = ['uibPaginationConfig'];
-
-function run(uibPaginationConfig) {
-    console.log('run');
-    //display First / Last buttons
-    uibPaginationConfig.boundaryLinks = true;
-    uibPaginationConfig.firstText = '第一页';
-    uibPaginationConfig.previousText = '上一页';
-    uibPaginationConfig.nextText = '下一页';
-    uibPaginationConfig.lastText = '最后一页';
-
-}
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,237 +1,104 @@
 let webpack = require('webpack');
-// inject bundle.js into html
 let HtmlWebpackPlugin = require('html-webpack-plugin');
-// inject bundle.css into html
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let CopyWebpackPlugin = require('copy-webpack-plugin');
-var autoprefixer = require('autoprefixer');
+let autoprefixer = require('autoprefixer');
 
 let ENV = process.env.npm_lifecycle_event;
 let isTest = ENV === 'test' || ENV === 'test-watch';
 let isProd = ENV === 'build';
 
 module.exports = function makeWebpackConfig() {
-    /**
-     * Config
-     * Reference: http://webpack.github.io/docs/configuration.html
-     * This is the object where all configuration gets set
-     */
-    var config = {};
 
-    /**
-     * Entry
-     * Reference: http://webpack.github.io/docs/configuration.html#entry
-     * Should be an empty object if it's generating a test build
-     * Karma will set this when it's a test build
-     */
-    config.entry = isTest ? {} : {
-        app: './src/js/app.js'
-    };
+	var config = {};
 
-    /**
-     * Output
-     * Reference: http://webpack.github.io/docs/configuration.html#output
-     * Should be an empty object if it's generating a test build
-     * Karma will handle setting it up for you when it's a test build
-     */
-    config.output = isTest ? {} : {
-        // Absolute output directory
-        path: __dirname + '/dist',
+	config.entry = isTest ? {} : {
+			app: './src/js/app.js'
+		};
 
-        // Output path from the view of the page
-        // Uses webpack-dev-server in development
-        publicPath: '',
+	config.output = isTest ? {} : {
+			path: __dirname + '/dist',
+			publicPath: '',
+			filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
+		};
 
-        // Filename for entry points
-        // Only adds hash in build mode
-        filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
-        // filename: '[name].bundle.js',
+	if (isTest) {
+		config.devtool = 'inline-source-map';
+	} else if (isProd) {
+		config.devtool = 'source-map';
+	} else {
+		config.devtool = 'eval-source-map';
+	}
 
-        // Filename for non-entry points
-        // Only adds hash in build mode
-        chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
-            // chunkFilename: '[name].bundle.js'
-    };
-
-    /**
-     * Devtool
-     * Reference: http://webpack.github.io/docs/configuration.html#devtool
-     * Type of sourcemap to use per build type
-     */
-    if (isTest) {
-        config.devtool = 'inline-source-map';
-    } else if (isProd) {
-        config.devtool = 'source-map';
-    } else {
-        config.devtool = 'eval-source-map';
-    }
-
-    /**
-     * Loaders
-     * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
-     * List: http://webpack.github.io/docs/list-of-loaders.html
-     * This handles most of the magic responsible for converting modules
-     */
-
-    // Initialize module
-    config.module = {
-        preLoaders: [],
-        loaders: [{
-                // JS LOADER
-                // Reference: https://github.com/babel/babel-loader
-                // Transpile .js files using babel-loader
-                // Compiles ES6 and ES7 into ES5 code
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/
-            }, {
-                // LESS LOADER
-                //Reference: https://www.npmjs.com/package/less-loader
-                test: /\.less$/,
-                // loader: ExtractTextPlugin.extract({ loader: ['raw-loader', 'less-loader'] })
-                // loader: 'style!css!less'
-                loader: ExtractTextPlugin.extract('style', 'css!postcss!less')
-                    // }, {
-                    //     // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-                    //     test: /\.tsx?$/,
-                    //     loader: 'ts'
-                    // }, {
-                    //     // CSS LOADER
-                    //     // Reference: https://github.com/webpack/css-loader
-                    //     // Allow loading css through js
-                    //     //
-                    //     // Reference: https://github.com/postcss/postcss-loader
-                    //     // Postprocess your css with PostCSS plugins
-                    //     test: /\.css$/,
-                    //     // Reference: https://github.com/webpack/extract-text-webpack-plugin
-                    //     // Extract css files in production builds
-                    //     //
-                    //     // Reference: https://github.com/webpack/style-loader
-                    //     // Use style-loader in development.
-                    //     loader: isTest ? 'null' : ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
-            }, {
-                // ASSET LOADER
-                // Reference: https://github.com/webpack/file-loader
-                // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
-                // Rename the file using the asset hash
-                // Pass along the updated reference to your code
-                // You can add here any file extension you want to get copied to your output
-                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-                loader: 'file?name=./assets/[name].[ext]'
-            }, {
-                // HTML LOADER
-                // Reference: https://github.com/webpack/raw-loader
-                // Allow loading html through js
-                test: /\.html$/,
-                loader: 'raw'
-            },
-            {
-
-                test: /\.json$/,
-                loader: 'json'
-            }
-        ]
-    };
-
-    // ISPARTA LOADER
-    // Reference: https://github.com/ColCh/isparta-instrumenter-loader
-    // Instrument JS files with Isparta for subsequent code coverage reporting
-    // Skips node_modules and files that end with .test.js
-    if (isTest) {
-        config.module.preLoaders.push({
-            test: /\.js$/,
-            exclude: [
-                /node_modules/,
-                /\.spec\.js$/
-            ],
-            loader: 'isparta-instrumenter'
-        });
-    }
-
-    /**
-     * PostCSS
-     * Reference: https://github.com/postcss/autoprefixer-core
-     * Add vendor prefixes to your css
-     */
-    config.postcss = [
-        autoprefixer({
-            browsers: ['> 5%']
-        })
-    ];
-
-    /**
-     * Plugins
-     * Reference: http://webpack.github.io/docs/configuration.html#plugins
-     * List: http://webpack.github.io/docs/list-of-plugins.html
-     */
-    config.plugins = [];
-
-    // Skip rendering index.html in test mode
-    if (!isTest) {
-        // Reference: https://github.com/ampedandwired/html-webpack-plugin
-        // Render index.html
-        config.plugins.push(
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-                chunks: ['app'],
-                inject: 'body'
-            }),
+	config.module = {
+		rules: [{
+			test: /\.js?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			options: {
+				presets: ["react"]
+			},
+		}, {
+			test: /\.less$/,
+			loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: ['css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [
+								autoprefixer({
+									browsers: ['> 5%']
+								})
+							]
+						}
+					},
+					'less-loader']
+			})
+		}, {
+			test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+			loader: 'file-loader?name=./assets/[name].[ext]'
+		}, {
+			test: /\.html$/,
+			loader: 'raw-loader'
+		}, {
+			test: /\.json$/,
+			loader: 'json-loader'
+		}
+		]
+	};
 
 
-            // Reference: https://github.com/webpack/extract-text-webpack-plugin
-            // Extract css files
-            // Disabled when in test mode or not in build mode
-            // [name].[hash].css
-            new ExtractTextPlugin('[name].[hash].css', { disable: !isProd })
-        );
-    }
+	config.plugins = [];
 
-    // Add build specific plugins
-    if (isProd) {
-        config.plugins.push(
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
-            // Only emit files when there are no errors
-            new webpack.NoErrorsPlugin(),
+	if (!isTest) {
+		config.plugins.push(
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+				inject: 'body'
+			}),
 
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-            // Dedupe modules in the output
-            new webpack.optimize.DedupePlugin(),
+			new ExtractTextPlugin({filename: '[name].[hash].css', disable: !isProd})
+		);
+	}
 
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-            // Minify all javascript, switch loaders to minimizing mode
-            new webpack.optimize.UglifyJsPlugin(),
+	if (isProd) {
+		config.plugins.push(
+			new webpack.NoErrorsPlugin(),
+			new webpack.optimize.UglifyJsPlugin(),
+			new CopyWebpackPlugin([{
+				from: 'src/images',
+				to: 'images'
+			}])
+		);
+	}
 
-            // Copy assets from the public folder
-            // Reference: https://github.com/kevlened/copy-webpack-plugin
-            new CopyWebpackPlugin([{
-                from: 'src/images',
-                to: 'images'
-                    // },{
-                    // from: 'api',
-                    // to:'api'
-            }], {
-                ignore: [
-                    // Doesn't copy composer
-                    '*.phar',
-                ],
+	config.devServer = {
+		port: 3200,
+		contentBase: './src',
+		watchContentBase: true,
+		open: true
+	};
 
-                // By default, we only copy modified files during
-                // a watch or webpack-dev-server build. Setting this
-                // to `true` copies all files.
-                copyUnmodified: true
-            })
-        );
-    }
-
-    /**
-     * Dev server configuration
-     * Reference: http://webpack.github.io/docs/configuration.html#devserver
-     * Reference: http://webpack.github.io/docs/webpack-dev-server.html
-     */
-    config.devServer = {
-        contentBase: './src',
-        stats: 'minimal'
-    };
-
-    return config;
+	return config;
 }();
