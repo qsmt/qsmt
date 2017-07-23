fb857e6c1010e9d9829751bbce8fa50987c94ef0
shengoo <shengoo@hotmail.com>
2017-07-23 23:12:37 +0800

    partners page

--- src/js/components/portfolio/Portfolio.js
+++ src/js/components/portfolio/Portfolio.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class Portfolio extends Component{
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true
+		};
+	}
+
 	static path = '/portfolio';
+
+	componentDidMount() {
+		fetch('http://www.qsmttech.com/wp-json/wp/v2/posts?categories=2&orderby=slug&order=asc&page=1&per_page=100')
+			.then(response => response.json())
+			.then(json => {
+				this.setState({items: json, loading: false, select: 0});
+			})
+	}
 
     render(){
         return(
