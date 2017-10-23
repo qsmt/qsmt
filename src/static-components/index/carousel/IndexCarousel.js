a008814ed0dd7afbb1f8867101e53b0f15b8447b
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-10-23 17:14:04 +0800

    d

--- src/static-components/index/carousel/IndexCarousel.js
+++ src/static-components/index/carousel/IndexCarousel.js
@@ -7,7 +7,6 @@ import image2 from './carousel3.png';
 import image3 from './carousel4.png';
 import image4 from './carousel5.png';
 import image5 from './carousel6.png';
-const slides = require('./data/carousels.json');
 const images = [
 	image0,image1,image2,image3,image4,image5
 ];
@@ -16,9 +15,9 @@ const images = [
 class IndexCarousel extends PureComponent {
 
 	render() {
-		const items = slides.map((item,index) =>{
+		const items = images.map((item,index) =>{
 
-			return <Carousel.Item key={item.id}>
+			return <Carousel.Item key={index}>
 				<img src={images[index]} alt={''}/>
 				{/*<Carousel.Caption>*/}
 				{/*<h3>First slide label</h3>*/}
