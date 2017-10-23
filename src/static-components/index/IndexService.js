dc18b4f68c7ea6c522ff313654b3b4750633167b
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-10-23 17:29:58 +0800

    d

--- src/static-components/index/IndexService.js
+++ src/static-components/index/IndexService.js
@@ -1,5 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {PageTitle} from '../../components';
+
+const services = [
+    {
+        title: '专注领域',
+        content: [
+            '世博会、奥运会展示场馆',
+            '科技馆、博物馆',
+            '各类主题场馆',
+            '城市规划馆、企业展示中心',
+            '青少年宫、妇女儿童活动中心、校史馆、纪念馆',
+        ]
+    },
+    {
+        title: '概念策划',
+        content: [
+            '头脑风暴、专家咨询',
+            '组织文案策划、资深设计师实地考察',
+            '拟定初步大纲、图纸绘制'
+        ]
+    },
+    {
+        title: '初步设计',
+        content: [
+            '展馆风格定位',
+            '绘制图纸、确定文字',
+            '资深顾问团队打造独特设计方案',
+        ]
+    },
+    {
+        title: '深化设计',
+        content: [
+            '装饰设计深化、系统整合空间装饰、展品与模型、完整施工图',
+            '展品文字、图形、脚本文字',
+            '资格预审、展品制作、多媒体内容制作、硬件集成'
+        ]
+    },
+    {
+        title: '工程实施',
+        content: [
+            '基础装饰、艺术布展、模型制作、强弱电布控、灯光音响调试、硬件设备安装、美工设计',
+            '项目经理进度推进,严把施工质量'
+        ]
+    },
+    {
+        title: '运营维护',
+        content: [
+            '主场服务、培训支持、问题指导',
+            '后期服务、电话回访',
+            '提供升级改造方案'
+        ]
+    }
+];
 
 class IndexService extends Component {
     render() {
@@ -10,93 +62,37 @@ class IndexService extends Component {
                         title={'我们的服务'}
                         description={'创新的策划理念 专业的实施团队 丰富的项目经验'}
                     />
-                    <div className="row">
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon1">
-                            </div>
-                            <div className="service-item-title">
-                                专注领域
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>世博会、奥运会展示场馆</li>
-                                    <li>科技馆、博物馆</li>
-                                    <li>各类主题场馆</li>
-                                    <li>城市规划馆、企业展示中心</li>
-                                    <li>青少年宫、妇女儿童活动中心、校史馆、纪念馆</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon2" />
-                            <div className="service-item-title">
-                                概念策划
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>头脑风暴、专家咨询</li>
-                                    <li>组织文案策划、资深设计师实地考察</li>
-                                    <li>拟定初步大纲、图纸绘制</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon3" />
-                            <div className="service-item-title">
-                                初步设计
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>展馆风格定位</li>
-                                    <li>绘制图纸、确定文字</li>
-                                    <li>资深顾问团队打造独特设计方案</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon4" />
-                            <div className="service-item-title">
-                                深化设计
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>装饰设计深化、系统整合空间装饰、展品与模型、完整施工图</li>
-                                    <li>展品文字、图形、脚本文字</li>
-                                    <li>资格预审、展品制作、多媒体内容制作、硬件集成</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon5" />
-                            <div className="service-item-title">
-                                工程实施
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>基础装饰、艺术布展、模型制作、强弱电布控、灯光音响调试、硬件设备安装、美工设计</li>
-                                    <li>项目经理进度推进,严把施工质量</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
-                            <div className="service-item-icon service-item-icon6" />
-                            <div className="service-item-title">
-                                运营维护
-                            </div>
-                            <div className="service-item-content">
-                                <ul>
-                                    <li>主场服务、培训支持、问题指导</li>
-                                    <li>后期服务、电话回访</li>
-                                    <li>提供升级改造方案</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    <ServiceList data={services}/>
                 </div>
             </div>
         );
     }
 }
+
+const ServiceList = ({data}) => (
+    data.map(item => (
+        <div className="row">
+            <ServiceItem data={item}/>
+        </div>
+    ))
+);
+
+const ServiceItem = ({data}) => (
+    <div className="col-lg-2 col-sm-4 col-xs-6 service-item">
+        <div className="service-item-icon service-item-icon1">
+        </div>
+        <div className="service-item-title">
+            {data.title}
+        </div>
+        <div className="service-item-content">
+            <ul>
+                {data.content.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+);
 
 
 export default IndexService;
