09d0d83308d0e2c0a3dba70521510551337af2d6
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-10-24 11:32:24 +0800

    add key

--- src/static-components/about/About.js
+++ src/static-components/about/About.js
@@ -1,25 +1,40 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 
 import img from './about.jpg';
+import image1 from './1.jpg';
+import image2 from './2.png';
+import image3 from './3.jpg';
+import image4 from './4.png';
 
-class About extends PureComponent {
+const data = [
+	{
+		title: '公司简介',
+		image: image1,
+	},
+	{
+		title: '资质荣誉',
+		image: image2
+	},
+    {
+        title: '专注领域',
+        image: image3
+    },
+    {
+        title: '优势价值',
+        image: image4
+    }
+];
+
+class About extends React.PureComponent {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			loading: true
+            select: 0
 		};
 	}
 
 	static path = '/about';
-
-	// componentDidMount() {
-	// 	fetch('http://www.qsmttech.com/wp-json/wp/v2/posts?categories=2&orderby=slug&order=asc&page=1&per_page=100')
-	// 		.then(response => response.json())
-	// 		.then(json => {
-	// 			this.setState({items: json, loading: false, select: 0});
-	// 		})
-	// }
 
 	render() {
 		return (
@@ -28,15 +43,12 @@ class About extends PureComponent {
 					<img src={img} style={{width: '100%', height: '20vw'}} alt={''}/>
 				</div>
 				<div className="loading-container">
-					{this.state.loading ?
-						<div className="loading"/> :
-						<div className="container">
-							<div className="row">
-								{this.state.items && this._renderNav()}
-								{this._renderContent()}
-							</div>
+					<div className="container">
+						<div className="row">
+							{this._renderNav()}
+							{this._renderContent()}
 						</div>
-					}
+					</div>
 				</div>
 			</div>
 		)
@@ -49,17 +61,17 @@ class About extends PureComponent {
 	}
 
 	_renderNav() {
-		const nav = this.state.items.map((item, index) => {
+		const nav = data.map((item, index) => {
 			const className = index === this.state.select ?
 				"col-xs-3 col-sm-12 repeated-item active" :
 				"col-xs-3 col-sm-12 repeated-item";
 			return (
-				<li key={item.id} className={className}>
+				<li key={index} className={className}>
 					<a //href="javascript:void(0)"
-					   onClick={e => this.handleClick(index)}>{item.title.rendered}</a>
+					   onClick={e => this.handleClick(index)}>{item.title}</a>
 				</li>
 			)
-		})
+		});
 		return (
 			<div className="col-xs-12 col-sm-2">
 				<ul className="nav nav-pills qsmt-sub-nav">
@@ -74,16 +86,14 @@ class About extends PureComponent {
 			<div className="col-xs-12 col-sm-10">
 				<div className="row">
 					<div className="page-title col-xs-12">
-						<h1>{this.state.items[this.state.select].title.rendered}</h1>
+						<h1>{data[this.state.select].title}</h1>
 						<div className="divider"/>
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-xs-12"
-						 dangerouslySetInnerHTML={{
-							 __html: this.state.items[this.state.select].content.rendered
-						 }}
-					/>
+					<div className="col-xs-12">
+						<img src={data[this.state.select].image} alt={''} />
+					</div>
 				</div>
 			</div>
 		)
